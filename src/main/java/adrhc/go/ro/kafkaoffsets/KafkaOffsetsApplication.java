c89886aa4422e04301dd673d637e21625938d0e3
adrhc <adrhc@outlook.com>
2020-06-06 11:27:23 +0300

    init

---
+++ src/main/java/adrhc/go/ro/kafkaoffsets/KafkaOffsetsApplication.java
@@ -0,0 +1,13 @@
+package adrhc.go.ro.kafkaoffsets;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class KafkaOffsetsApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(KafkaOffsetsApplication.class, args);
+	}
+
+}
---
+++ src/test/java/adrhc/go/ro/kafkaoffsets/KafkaOffsetsApplicationTests.java
@@ -0,0 +1,13 @@
+package adrhc.go.ro.kafkaoffsets;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class KafkaOffsetsApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
