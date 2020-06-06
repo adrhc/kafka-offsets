1e9a8d9b745db7aeb0cdb4c3f2088f6e637be32d
adrhc <adrhc@outlook.com>
2020-06-06 15:05:28 +0300

    offset exceeded test

--- src/test/java/adrhc/go/ro/kafkaoffsets/OrderConsumerIT.java
+++ src/test/java/adrhc/go/ro/kafkaoffsets/OrderConsumerIT.java
@@ -10,9 +10,12 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.beans.factory.annotation.Value;
 import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.kafka.core.ConsumerFactory;
+import org.springframework.test.util.ReflectionTestUtils;
 
 import java.time.Duration;
 import java.util.List;
+
+import static org.assertj.core.api.Assertions.assertThat;
 
 @SpringBootTest
 @EnabledIfSystemProperty(named = "integration", matches = "true")
@@ -30,9 +33,12 @@ public class OrderConsumerIT {
 		try (Consumer<String, Order> consumer = consumerFactory.createConsumer()) {
 			consumer.subscribe(List.of(properties.getOrders()));
 			consumer.poll(Duration.ofSeconds(5))
-					.forEach(it -> log.debug("\n{}", it.value()));
+					.forEach(it -> {
+						log.debug("\n{}", it.value());
+						assertThat(it.offset()).withFailMessage("offset exceeded").isLessThan(maxPollRecords);
+					});
 			log.debug("skipping consumer auto closure");
-			System.exit(0);
+			ReflectionTestUtils.setField(consumer, "closed", Boolean.TRUE);
 		}
 	}
 }
